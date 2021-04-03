32b87e4c4cf1b0c800113aa50ff424b8ace101ba
attila5287 <atiturkoz@hotmail.com>
2021-04-03 13:01:40 -0600

    first run on local chk hero

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('all', { logged_in: req.session.logged_in, user_id : req.session.user_id });
 });
 
 module.exports = router;
