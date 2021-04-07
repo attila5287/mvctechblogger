acb3afd6e253ca8fe7c4539365acdf8a0e276944
attila5287 <atiturkoz@hotmail.com>
2021-04-06 19:40:38 -0600

    updates post

--- public/js/update_post.js
+++ public/js/update_post.js
@@ -1,15 +1,16 @@
 const update_post = async (event) => {
   event.preventDefault();
 
+  const post_id = document.querySelector('#post_id').value.trim();
   const user_id = document.querySelector('#user_id').value.trim();
   const category_id = document.querySelector('#category_id').value.trim();
   const title = document.querySelector('#title').value.trim();
   const content = document.querySelector('#content').value.trim();
 
   if (category_id && title  && content) {
-    const response = await fetch('/post', {
+    const response = await fetch('/post/'+post_id, {
       method: 'PUT',
-      body: JSON.stringify({ category_id, title, content }),
+      body: JSON.stringify({ user_id, category_id, title, content }),
       headers: { 'Content-Type': 'application/json' },
     });
     
