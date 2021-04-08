f8e8098c2b0068e509ba58854c9d52b31ee75ba4
attila5287 <atiturkoz@hotmail.com>
2021-04-08 17:36:54 -0600

    fx dash style

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-const paginate = require('express-paginate');
+const paginate = require( 'express-paginate' );
 const withAuth = require( '../utils/auth' );
 // const path = require( 'path' );
 const router = require( 'express' ).Router();
@@ -16,24 +16,23 @@ const {
   Usercat
 } = require( '../models' );
 
-router.post('/filter', async (req, res) => {
-  
+router.post( '/filter', async ( req, res ) => {
+  console.log( req.body );
   const posts_q = await Post.findAll( {
     include: {
       all: true,
       nested: true,
-    }
-    , where: {
+    },
+    where: {
       category_id: req.body.cats
     }
-  }
-   ).catch( e => console.log(e));
+  } ).catch( e => console.log( e ) );
 
   const all = posts_q.map( p => p.get( {
     plain: true
   } ) );
   // res.json( posts );
- 
+
   const cats_models = await Category.findAll().catch( e => console.log( e ) );
   const cats = cats_models.map( c => c.get( {
     plain: true
@@ -42,44 +41,45 @@ router.post('/filter', async (req, res) => {
   res.render( 'all', {
     all,
     cats,
-    at_home : req.path == '/',
+    at_home: req.path == '/',
     logged_in: req.session.logged_in,
     user_id: req.session.user_id,
   } );
-});
+} );
 
 router.get( '/', async ( req, res ) => {
 
- 
   const post_m = await Post.findAll( {
-    limit: req.query.limit, offset: req.skip,
+    limit: req.query.limit,
+    offset: req.skip,
     include: {
       all: true,
       nested: true,
     },
-  }
-   ).catch( e => console.log(e));
-   const all = post_m.map(p=> p.get( {plain: true} ));
-   const itemCount = post_m.count;
-  console.log('itemC :>> ', itemCount);
+  } ).catch( e => console.log( e ) );
+  const all = post_m.map( p => p.get( {
+    plain: true
+  } ) );
+  const itemCount = post_m.count;
+  console.log( 'itemC :>> ', itemCount );
   const pageCount = Math.ceil( post_m.count / req.query.limit );
-  console.log('\npagecount :>> ', pageCount);
-  
- 
+  console.log( '\npagecount :>> ', pageCount );
+
+
   const cats_models = await Category.findAll().catch( e => console.log( e ) );
   const cats = cats_models.map( c => c.get( {
     plain: true
   } ) );
   // res.status(200).json(post_m);
   res.render( 'all', {
-      all,
-      cats,
+    all,
+    cats,
     logged_in: req.session.logged_in,
     user_id: req.session.user_id,
-    at_home : req.path == '/',
+    at_home: req.path == '/',
     // pageCount,
     // itemCount,
-    pages: paginate.getArrayPages(req)(3, pageCount, req.query.page)
+    pages: paginate.getArrayPages( req )( 3, pageCount, req.query.page )
   } );
 } );
 
@@ -102,12 +102,11 @@ router.get( '/dashboard/:id', async ( req, res ) => {
     include: {
       all: true,
       nested: true,
-    }
-    , where: {
+    },
+    where: {
       user_id: req.params.id
     }
-  }
-   ).catch( e => console.log(e));
+  } ).catch( e => console.log( e ) );
 
   const posts = users_posts.map( p => p.get( {
     plain: true
